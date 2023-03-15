56f572828078078188875b52c315e645d10e0b6a
rana-093 <masud@priyo.com>
2023-03-15 16:03:10 +0600

    Added the basicic skeleton of terraform

--- aws/modules/vpc/variables.tf
+++ aws/modules/vpc/variables.tf
@@ -1,5 +1,5 @@
-variable "region" { ap-southeast-1 }
-variable "cidr_block" { 10.0.0.0/16 }
+variable "region" {}
+variable "cidr_block" {}
 # variable "cidr_block" {}
 # variable "cidr_block" {}
 # variable "cidr_block" {}
