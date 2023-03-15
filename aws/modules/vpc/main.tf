5b3866ee29fabddd29ff15f9bc5ad23568e92478
rana-093 <masud@priyo.com>
2023-03-15 15:56:51 +0600

    Added the basicic skeleton of terraform

---
+++ aws/modules/vpc/main.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "demoVpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "demoVpc"
+  }
+}
---
+++ aws/modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable "region" { ap-southeast-1 }
+variable "cidr_block" { 10.0.0.0/16 }
+# variable "cidr_block" {}
+# variable "cidr_block" {}
+# variable "cidr_block" {}
+# variable "cidr_block" {}
